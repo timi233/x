540c700c3a3c0a5b06f2559aec7169c4280919c5
Dustin Brett <dustin@voxter.ca>
2020-12-01 21:36:08 -0800

    Work on toolbar

--- components/System/WindowManager/TitleBar.tsx
+++ components/System/WindowManager/TitleBar.tsx
@@ -14,25 +14,31 @@ const TitleBar: React.FC<TitleBarProps> = ({
   onClose
 }) => {
   return (
-    <header className={`${styles.titlebar} handle`} onDoubleClick={onMaximize}>
-      <h1>
-        <figure>
-          <Icon src={icon} height={16} width={16} />
-          <figcaption>{name}</figcaption>
-        </figure>
-      </h1>
-      <nav className="cancel">
-        <button id={styles.close} type="button" onClick={onClose}>
-          <FontAwesomeIcon size="xs" icon={faTimes} />
-        </button>
-        <button id={styles.minimize} type="button" onClick={onMinimize}>
-          <FontAwesomeIcon size="xs" icon={faMinus} />
-        </button>
-        <button id={styles.maximize} type="button" onClick={onMaximize}>
-          <FontAwesomeIcon size="xs" icon={faPlus} />
-        </button>
+    <div className={styles.titlebar}>
+      <header className="handle" onDoubleClick={onMaximize}>
+        <h1>
+          <figure>
+            <Icon src={icon} height={16} width={16} />
+            <figcaption>{name}</figcaption>
+          </figure>
+        </h1>
+        <nav className="cancel">
+          <button id={styles.close} type="button" onClick={onClose}>
+            <FontAwesomeIcon size="xs" icon={faTimes} />
+          </button>
+          <button id={styles.minimize} type="button" onClick={onMinimize}>
+            <FontAwesomeIcon size="xs" icon={faMinus} />
+          </button>
+          <button id={styles.maximize} type="button" onClick={onMaximize}>
+            <FontAwesomeIcon size="xs" icon={faPlus} />
+          </button>
+        </nav>
+      </header>
+      <nav className={`${styles.toolbar} handle`}>
+        <button type="button">&lt;</button>
+        <button type="button" className={styles.enabled}>&gt;</button>
       </nav>
-    </header>
+    </div>
   );
 };
 
