1ff31a17c15e5d0c48eb699ddf82c39cacd9c4e1
Dustin Brett <dustinbrett@gmail.com>
2021-01-13 08:32:05 -0800

    Another tweak

--- __tests__/pages/index.spec.tsx
+++ __tests__/pages/index.spec.tsx
@@ -1,13 +1,14 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import StyledApp from 'components/pages/StyledApp';
 import Index from 'pages/index';
 
-test('renders index page', () => {
+test('renders index page', async () => {
   const { getByText } = render(
     <StyledApp>
       <Index />
     </StyledApp>
   );
+  const helloWorldElement = await waitFor(() => getByText('Hello, world!'));
 
-  expect(getByText('Hello, world!')).toBeInTheDocument();
+  expect(helloWorldElement).toBeInTheDocument();
 });
