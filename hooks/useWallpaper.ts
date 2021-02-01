52774bcf17bebf136b98832d7ba85160f84d07f2
Dustin Brett <dustinbrett@gmail.com>
2021-01-31 23:14:22 -0800

    Check for WebGL to fix test

--- hooks/useWallpaper.ts
+++ hooks/useWallpaper.ts
@@ -3,16 +3,18 @@ import * as THREE from 'three';
 /* @ts-expect-error No declaration file required */
 import WAVES from 'vanta/dist/vanta.waves.min';
 
+const isWebGLAvailable = typeof WebGLRenderingContext !== 'undefined';
+
 const useWallpaper = (desktopRef: React.RefObject<HTMLElement>): void => {
   useEffect(() => {
-    const vantaEffect = WAVES({
-      el: desktopRef.current,
-      THREE // NOTE: @ v124 until Vanta.js uses THREE.BufferGeometry
-    });
+    const vantaEffect =
+      isWebGLAvailable &&
+      WAVES({
+        el: desktopRef.current,
+        THREE // NOTE: @ v124 until Vanta.js uses THREE.BufferGeometry
+      });
 
-    return () => {
-      vantaEffect.destroy();
-    };
+    return () => vantaEffect?.destroy?.();
   }, [desktopRef]);
 };
 
