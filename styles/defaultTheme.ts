d779c824c6919e1f89f147ba7d81bbc23a14105f
Dustin Brett <dustinbrett@gmail.com>
2021-02-07 20:43:11 -0800

    Extract defaultTheme

---
+++ styles/defaultTheme.ts
@@ -0,0 +1,18 @@
+import vantaWaves from 'utils/vantaWaves';
+
+const defaultTheme = {
+  colors: {
+    backgroundcolor: '#000',
+    primary: '#000',
+    window: '#808080'
+  },
+  wallpaper: vantaWaves({
+    color: 0x5588,
+    shininess: 35,
+    waveHeight: 15,
+    waveSpeed: 0.3,
+    zoom: 0.9
+  })
+};
+
+export default defaultTheme;
