1369bda4068afa0029df42cd8719c5f1194e6453
Dustin Brett <dustinbrett@gmail.com>
2021-02-11 19:57:28 -0800

    Desktop tweaks like role

--- styles/components/system/StyledDesktop.ts
+++ styles/components/system/StyledDesktop.ts
@@ -3,7 +3,6 @@ import styled from 'styled-components';
 const StyledDesktop = styled.main`
   background-color: ${({ theme }) => theme.colors.background};
   bottom: 0;
-  display: flow-root;
   height: 100vh;
   left: 0;
   position: absolute;
