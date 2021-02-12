d5a9ec4a4cbeb9b6437b7ee77c4e648160c5464a
Dustin Brett <dustinbrett@gmail.com>
2021-02-11 20:11:08 -0800

    Only arrow functions

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,16 +3,13 @@ import StyledApp from 'components/pages/StyledApp';
 import { SessionProvider } from 'contexts/session';
 import type { AppProps } from 'next/app';
 
-export default function App({
-  Component,
-  pageProps
-}: AppProps): React.ReactElement {
-  return (
-    <SessionProvider>
-      <StyledApp>
-        <Metadata />
-        <Component {...pageProps} />
-      </StyledApp>
-    </SessionProvider>
-  );
-}
+const App = ({ Component, pageProps }: AppProps): React.ReactElement => (
+  <SessionProvider>
+    <StyledApp>
+      <Metadata />
+      <Component {...pageProps} />
+    </StyledApp>
+  </SessionProvider>
+);
+
+export default App;
